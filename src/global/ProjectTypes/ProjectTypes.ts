bb2b117566035cb7be99dcedf156c629d2a64824
jgirish23 <jakkalagirish@gmail.com>
2025-12-25 06:38:17 +0530

    kept only UI code

---
+++ src/global/ProjectTypes/ProjectTypes.ts
@@ -0,0 +1,6 @@
+export const ProjectTypes = [
+    { value: '0', text: 'Select Project Name' },
+    { value: '1', text: 'React js' },
+    { value: '2', text: 'Python' },
+    { value: '3', text: 'javascript' }
+  ];
